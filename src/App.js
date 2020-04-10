a0828a4631528a104b3905bdaab9281a06b2f7fb
Kiran LM <kiran.lm96@gmail.com>
2020-04-11 01:57:05 +0530

    google and facebook auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useReducer, useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import appSyncConfig from './aws-exports';
@@ -6,14 +6,70 @@ import { ApolloProvider } from 'react-apollo';
 import AWSAppSyncClient from 'aws-appsync';
 import { Rehydrated } from 'aws-appsync-react';
 import Dashboard from './Components/Dashboard';
+import Login from './Components/Login';
+import Amplify, { Hub, Auth } from 'aws-amplify';
 
-const App = () => (
-  <Router>
-    <div>
-      <Route path="/" component={Dashboard} />
-    </div>
-  </Router>
-);
+Amplify.configure(appSyncConfig);
+
+const initialUserState = { user: null, loading: true };
+
+const App = () => {
+  const [userState, dispatch] = useReducer(reducer, initialUserState);
+
+  useEffect(() => {
+    // set listener for auth events
+    Hub.listen('auth', (data) => {
+      const { payload } = data;
+      if (payload.event === 'signIn') {
+        dispatch({ type: 'setUser', user: payload.data });
+      }
+      // this listener is needed for form sign ups since the OAuth will redirect & reload
+      if (payload.event === 'signOut') {
+        dispatch({ type: 'setUser', user: null });
+      }
+    });
+    checkUser(dispatch);
+  }, []);
+
+  return (
+    <Router>
+      <div>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/" component={Dashboard} />
+      </div>
+    </Router>
+  );
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'setUser':
+      return { ...state, user: action.user, loading: false };
+    case 'loaded':
+      return { ...state, loading: false };
+    default:
+      return state;
+  }
+}
+
+async function checkUser(dispatch) {
+  try {
+    const user = await Auth.currentAuthenticatedUser();
+    console.log('user: ', user);
+    dispatch({ type: 'setUser', user });
+  } catch (err) {
+    console.log('err: ', err);
+    dispatch({ type: 'loaded' });
+  }
+}
+
+function signOut() {
+  Auth.signOut()
+    .then((data) => {
+      console.log('signed out: ', data);
+    })
+    .catch((err) => console.log(err));
+}
 
 const client = new AWSAppSyncClient({
   url: appSyncConfig.aws_appsync_graphqlEndpoint,
